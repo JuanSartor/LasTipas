30d5c9233ae7439834f546322f6fccd560f20bad
juanPcEscritorio <juansartor92@gmail.com>
2021-06-12 23:58:37 -0300

    avance con la tabla de comandas, debo seguir con la tabla de detalle

---
+++ tablaComandas.php
@@ -0,0 +1,125 @@
+
+<?php
+     session_start(); 
+
+
+include ("clases/conexion.php");
+
+$conexion=conectar();
+
+// el estado 0(cero) indica que la comanda todavia no fue cancelada, ni preparada, es decir esta en espera
+
+$sql="SELECT id,mesas,estado FROM comandas where estado=0  and fecha_creacion > DATE_SUB(CURDATE(), INTERVAL 1 DAY)";
+
+$result = mysqli_query($conexion,$sql);
+
+
+$fechaActual= getdate();
+
+$fechaDeHoy=$fechaActual['mday'].'-'.$fechaActual['mon'].'-'.$fechaActual['year'];
+
+
+
+?>
+
+
+
+
+
+<div>
+	<link href="librerias/bootstrap/tableexport.css" rel="stylesheet" type="text/css">
+	
+	<table class="table table-hover table-condensed table-bordered" id="iddatatable">
+
+		<thead  style="background-color:#ccc; color: white; font-weight: bold; ">
+			<tr>
+				<td style="font-size: 12px" >Moso</td>
+				<td style="font-size: 12px" >Mesas</td>
+				<td style="font-size: 12px">Estado</td>
+			
+				
+				<td style="font-size: 12px">Detalles</td>
+				
+
+			</tr>
+
+
+		</thead>
+
+		
+			<tbody>
+				<?php
+				while ($mostrar=mysqli_fetch_array($result)) {
+			
+		
+
+					?>
+
+				<tr style="background-color: white;">
+					<td style="font-size: 12px"> <?php echo $_SESSION["usernameC"]?> </td>
+					<td style="font-size: 12px"> <?php echo $mostrar[1]?> </td>
+				
+				     <td style="font-size: 12px"> <?php 
+				    	if($mostrar[2]=='0'){
+				    		echo "<div class=\"alert alert-success\" role=\"alert\" style=\"height:35px\">En Espera</div> "; 
+				    	}
+				    		else{echo "<div class=\"alert alert-danger\" role=\"alert\" style=\"height:35px\" >Terminada</div> "; }
+
+
+				   ?> </td> 
+					 
+					
+						<td> 
+						<span class="btn btn-warning btn-sm" data-toggle="modal" title="Detalles"  data-target="#modalEditar" onclick="actualizarUsuario('<?php echo $mostrar[6] ?>')">
+					Detalles
+						</span>
+						</td>  
+						
+
+					
+					
+
+				</tr>
+
+
+
+
+
+
+
+<?php
+}
+?>
+			</tbody>
+
+
+	</table>
+</div>
+
+
+<script type="text/javascript">
+	
+$(document).ready(function() {
+    $('#iddatatable').DataTable();
+} );
+
+</script>
+
+<script src="librerias/tableExport/FileSaver.min.js"></script>
+<script src="librerias/tableExport/Blob.min.js"></script>
+<script src="librerias/tableExport/xls.core.min.js"></script>
+<script src="librerias/tableExport/tableexport.js"></script>
+<script>
+$("table").tableExport({
+	formats: ["xlsx"],//Tipo de archivos a exportar ("xlsx","txt", "csv", "xls")
+	ignoreCols: [6,7], 
+	position: 'button',  // Posicion que se muestran los botones puedes ser: (top, bottom)
+	bootstrap: false,//Usar lo estilos de css de bootstrap para los botones (true, false)
+	fileName: "Usuarios al <?php echo $fechaDeHoy;?>",    //Nombre del archivo 
+});
+</script>
+
+
+
+
+
