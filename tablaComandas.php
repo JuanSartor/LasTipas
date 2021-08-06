b42e9d829a621b93b3068eb630da44f869a4c206
Sartor Juan <juansartor92@gmail.com>
2021-08-06 00:53:03 -0300

    seguir con lo de las mesas q todavia no pudiste solucionar

--- tablaComandas.php
+++ tablaComandas.php
@@ -10,16 +10,130 @@ $conexion=conectar();
 // el estado 0(cero) indica que la comanda todavia no fue cancelada, ni preparada, es decir esta en espera
 
 $sql="SELECT c.id, c.mesas, c.estado, u.nombre, u.apellido  FROM comandas c, usuarios u, mesas m
-where c.estado=0 and c.id_usuario_logueado='$_SESSION[idC]'  and u.id=c.id_usuario_logueado  ";
-
-
-
-// quedaste aca arriba tenes q corregir xq se ve el id de las mesas y tiene  q mostrarse el NUMERO de mesa
-// despues de esto hacer pruebas completa de la logica
-
+where c.estado=0 and c.id_usuario_logueado='$_SESSION[idC]'  and u.id=c.id_usuario_logueado and c.mesas=m.id ";
 
 
 $result = mysqli_query($conexion,$sql);
+
+
+$arregloPrevioAString=  Array();
+$i=0;
+
+$arregloCantidaEleEnPos1= Array();
+$pos=0;
+
+while ($mostrar=mysqli_fetch_array($result)) {
+
+	$arregloCantidaEleEnPos1[$pos]=sizeof(explode(",",$mostrar[1]));
+
+	if( $arregloCantidaEleEnPos1[$pos] > 1){
+
+ $subArreglo=explode(",", $mostrar[1]); 
+ 		foreach($subArreglo as &$valor){
+
+			$arregloPrevioAString[$i]=$valor;
+			$i++;
+
+
+		}
+
+
+}
+	else{
+
+		$arregloPrevioAString[$i]=$mostrar[1];
+		$i++;
+
+	}
+	$pos++;
+
+
+}
+// print_r($arregloPrevioAString);
+
+
+
+
+$ids = "";
+
+foreach($arregloPrevioAString as &$valorids){
+	// Esto crea un string como 'id1','id2','id3',
+		$ids .= "'".$valorids . "', ";
+	}
+	// Esto quita el ultimo espacio y coma del string generado con lo cual
+	// el string queda 'id1','id2','id3'
+	$ids = substr($ids,0,-2);
+
+
+// print($ids);
+
+$consulta = "SELECT id,numero FROM mesas WHERE id in (".$ids.")"; 
+
+
+$resultadiIds = mysqli_query($conexion,$consulta);
+
+
+$contad=0;
+$arrayIds=Array();
+$contUl=0;
+$cad='';
+while ($mostrarids=mysqli_fetch_array($resultadiIds)) {
+
+	if($arregloCantidaEleEnPos1[$contad]>1){
+		
+
+		for($n=0; $n<$arregloCantidaEleEnPos1[$contad]; $n++){
+			
+			foreach($mostrarids as &$idC){
+				print_r($idC[0]);
+				if($idC[0] == $arregloPrevioAString[$contUl] ){
+					
+					
+					$cad=$cad.$idC[1].',';
+					
+				}
+	
+			}
+
+			$contUl++;
+
+		}
+
+		$cadenaFinal=substr($cad, 0, -1);
+		$arrayIds[$contad]=$cadenaFinal;
+
+		$contad++;
+
+		$cad='';
+
+
+	}
+	else{
+
+		foreach($mostrarids as &$idC){
+			if($idC[0] == $arregloPrevioAString[$contUl] ){
+				$arrayIds[$contad]=$idC[1];
+				
+			}
+
+		}
+		$contUl++;
+
+		$contad++;
+
+	}
+
+
+
+	
+
+ 
+}
+
+
+
+
+
 
 
 $fechaActual= getdate();
